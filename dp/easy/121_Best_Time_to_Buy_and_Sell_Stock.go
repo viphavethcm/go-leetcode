a280cb56b6197de9bb11ea65d9fcb557d107af56
viphavethcm <loivang.nhatduy@gmail.com>
2026-02-25 11:12:26 +0700

    Best time to buy and sell stock

---
+++ dp/easy/121_Best_Time_to_Buy_and_Sell_Stock.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	prices := []int{7, 1, 5, 3, 6, 4}
+	fmt.Println(maxProfit(prices))
+}
+
+func maxProfit(prices []int) int {
+	minPrice := math.MaxInt32
+	maxProfit := 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else {
+			maxProfit = max(maxProfit, prices[i]-minPrice)
+		}
+	}
+	return maxProfit
+}
