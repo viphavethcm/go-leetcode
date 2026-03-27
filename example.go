852c309a486481d22b7f0b96a11db22c59930c3e
duy.doan <duy.doan@be.com.vn>
2026-03-27 21:10:35 +0700

    example

---
+++ example.go
@@ -0,0 +1,27 @@
+package main
+
+//
+//import (
+//	"fmt"
+//)
+//
+//func main() {
+//	fmt.Println(findErrorNums([]int{3, 2, 3, 4, 6, 5}))
+//}
+//
+//func findErrorNums(nums []int) []int {
+//	rs := make([]int, 2)
+//	missing := 0
+//	for i := 0; i < len(nums); i++ {
+//		j := abs
+//
+//	}
+//	return rs
+//}
+//
+//func abs(x int) int {
+//	if x < 0 {
+//		return -x
+//	}
+//	return x
+//}
