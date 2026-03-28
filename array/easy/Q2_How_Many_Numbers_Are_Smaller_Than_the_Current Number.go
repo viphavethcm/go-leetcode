2730fcb8dac549ccc28b5298784d251a830196eb
duy.doan <duy.doan@be.com.vn>
2026-03-28 22:40:49 +0700

    Q2_How_Many_Numbers_Are_Smaller_Than_the_Current Number

--- array/easy/Q2_How_Many_Numbers_Are_Smaller_Than_the_Current Number.go
+++ array/easy/Q2_How_Many_Numbers_Are_Smaller_Than_the_Current Number.go
@@ -2,25 +2,48 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	fmt.Println(smallerNumbersThanCurrent([]int{6, 5, 4, 8}))
+	fmt.Println(smallerNumbersThanCurrent([]int{5, 0, 10, 0, 10, 6}))
 }
+
+// DP
+//func smallerNumbersThanCurrent(nums []int) []int {
+//	sorted := make([]int, len(nums))
+//	dp := make([]int, 101)
+//	copy(sorted, nums)
+//	sort.Ints(sorted)
+//	for i := 1; i < len(sorted); i++ {
+//		if sorted[i] == sorted[i-1] {
+//			continue
+//		}
+//		dp[sorted[i]] = i
+//	}
+//	for i := 0; i < len(nums); i++ {
+//		nums[i] = dp[nums[i]]
+//	}
+//	return nums
+//}
+
+// Prefix sum
 func smallerNumbersThanCurrent(nums []int) []int {
-	sorted := make([]int, len(nums))
-	dp := make([]int, 101)
-	copy(sorted, nums)
-	sort.Ints(sorted)
-	for i := 1; i < len(sorted); i++ {
-		if sorted[i] == sorted[i-1] {
-			continue
-		}
-		dp[sorted[i]] = i
-	}
+	dp := make([]int, 100)
+	// Count tần suất xuất hiện của các số trong array
 	for i := 0; i < len(nums); i++ {
-		nums[i] = dp[nums[i]]
+		dp[nums[i]]++
+	}
+	// Tìm tổng các số <= i = Số lần xuất hiện của i + Tổng các số <= i - 1
+	for i := 1; i < 100; i++ {
+		dp[i] += dp[i-1]
+	}
+	// Tìm số lần xuất hiện của i = Tổng các số <= i - 1 -> Tổng các số < 8 = Tổng các số <=7
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			nums[i] = 0
+		} else {
+			nums[i] = dp[nums[i]-1]
+		}
 	}
 	return nums
 }
